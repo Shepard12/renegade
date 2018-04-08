403a614034d57022cab1d8021ec7a5e281f40595
hozzy <hozzy1337@gmail.com>
2018-04-08 17:07:54 +0300

    Add: -AccountController(auth query processing) -Login and Register model(works with registration/data annotation) -Login, Register views(pages)

--- ren/ren/Controllers/HomeController.cs
+++ ren/ren/Controllers/HomeController.cs
@@ -5,14 +5,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ren.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ren.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
-            return View();
+            return Content(User.Identity.Name);
         }
 
         public IActionResult About()
